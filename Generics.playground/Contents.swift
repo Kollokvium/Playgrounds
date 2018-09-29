a0dc1432773e957e2789dbb2c4e50dc1e5e4a317
Anton Klymenko <klymenko.anton.job@gmail.com>
2018-09-30 01:45:10 +0300

    Numeric Protocol

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -14,6 +14,7 @@ func findInputIndex (_ array: [String], letter: String) -> Int? {
     return nil
 }
 
+// Comparable
 func findWithgeneric<T:Comparable> (_ array:[T], key:T) -> Int? {
     for (index, element) in array.enumerated() {
         if element == key {
@@ -26,3 +27,12 @@ func findWithgeneric<T:Comparable> (_ array:[T], key:T) -> Int? {
 findInputIndex(stringArray, letter: "h")
 findWithgeneric(stringArray, key: "h")
 findWithgeneric(integerArray, key: 42)
+
+// Numeric
+func anyNumberSum<E: Numeric>(lhs: E, rhs: E) -> E {
+    return lhs + rhs
+}
+
+anyNumberSum(lhs: 5, rhs: 2.34)
+anyNumberSum(lhs: -6, rhs: 10)
+anyNumberSum(lhs: 0.0000005, rhs: 1)
